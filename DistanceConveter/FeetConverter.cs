64fbf3bda33f377eba42fbbc45262667215faf1a
茂木 洸樹 <32090@ojs.ac.jp>
2021-05-25 10:20:30 +0900

    P52 リスト2.6 変換処理をクラス化

---
+++ DistanceConveter/FeetConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistanceConveter {
+    class FeetConverter {
+
+        //フィートからメートルを求める
+         public double FeetToMeter(int feet) {
+            return feet * 0.3048;
+        }
+
+        //メートルからフィートを求める
+         public double MeeterToFeet(int meter) {
+            return meter / 0.3048;
+        }
+    }
+}
--- DistanceConveter/Program.cs
+++ DistanceConveter/Program.cs
@@ -18,30 +18,23 @@ namespace DistanceConveter {
         }
         //メートルからフィートへの対応表
         private static void PrintMeterToFeetList(int start, int stop) {
-            for (int meter = 1; meter <= 10; meter++) {
-                double feet = MeeterToFeet(meter);
+            FeetConverter converter = new FeetConverter();
+            for (int meter = start; meter <= stop; meter++) {
+                double feet = converter.MeeterToFeet(meter);
                 Console.WriteLine("{0} m = {1:0.0000} ft", meter, feet);
 
             }
         }
         //フィートからメートルへの対応表
         private static void PrintFeetToMeterList(int start, int stop) {
-            for (int feet = 1; feet <= 10; feet++) {
-                double meter = FeetToMeter(feet);
+            FeetConverter converter = new FeetConverter();
+            for (int feet = start; feet <= stop; feet++) {
+                double meter = converter.FeetToMeter(feet);
                 Console.WriteLine("{0} ft = {1:0.0000} m", feet, meter);
 
             }
         }
 
-        //フィートからメートルを求める
-        private static double FeetToMeter(int feet) {
-            return feet * 0.3048;
-        }
-
-        //メートルからフィートを求める
-        private static double MeeterToFeet(int meter) {
-            return meter / 0.3048;
-        }
     }
 
 }
