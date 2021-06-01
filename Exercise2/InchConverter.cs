f2a5a560874f094e710cc231a07609f7e0247b2f
茂木 洸樹 <32090@ojs.ac.jp>
2021-06-01 10:26:08 +0900

    P79 問題2.2完成

---
+++ Exercise2/InchConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2 {
+    //インチとメートルの単位変換（静的クラス）
+     public static class InchConverter {
+        private const double ratio = 0.0254;
+
+        //メートルからインチを求める
+        public static double FromMeter(int meter) {
+            return meter * ratio;
+        }
+
+        //インチからメートルを求める
+        public static double ToMeter(int inch) {
+            return inch / ratio;
+        }
+    }
+}
--- Exercise2/Program.cs
+++ Exercise2/Program.cs
@@ -7,7 +7,32 @@ using System.Threading.Tasks;
 namespace Exercise2 {
     class Program {
         static void Main(string[] args) {
+            
+            if (args.Length >= 1 && args[0] == "-toi") {
+                // メートルからインチへの対応表
+                PrintMeterToInchList(1, 10);
+            } else {
+                //インチからメートルへの対応表
+                PrintInchToMeterList(1, 10);
+            }
 
+        }
+
+        private static void PrintInchToMeterList(int start, int stop) {
+            for (int inch = start; inch <= stop; inch++) {
+                double meter = InchConverter.FromMeter(inch);
+                Console.WriteLine("{0} inch = {1:0.0000} m", inch, meter);
+
+            }
+        }
+
+        private static void PrintMeterToInchList(int start, int stop) {
+            for (int meter = start; meter <= stop; meter++) {
+                double inch = InchConverter.ToMeter(meter);
+                Console.WriteLine("{0} m = {1:0.0000} inch", meter, inch);
+
+            }
+           
         }
     }
 }
