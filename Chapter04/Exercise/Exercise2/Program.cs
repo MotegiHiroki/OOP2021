4dd48860920a3f0d0147de3179b157db0e93a499
茂木 洸樹 <32090@ojs.ac.jp>
2021-06-11 15:25:18 +0900

    P134練習問題4.2完成

---
+++ Chapter04/Exercise/Exercise2/Program.cs
@@ -0,0 +1,44 @@
+﻿using Exercise1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2 {
+    class Program {
+
+        static void Main(string[] args) {
+
+           
+            var ym1 = new YearMonth(2019, 2);
+            var ym2 = new YearMonth(2017, 12);
+            var ym3 = new YearMonth(2016, 2);
+            var ym4 = new YearMonth(2013, 11);
+            var ym5 = new YearMonth(2019, 12);
+            var ym = new YearMonth[] { ym1, ym2, ym3, ym4, ym5 };
+
+            for (int i = 0; i < ym.Length; i++) {
+                Console.WriteLine(ym[i]);
+            }
+            
+            string a = ym.Contains(Is21(ym)) ? Is21(ym).Year.ToString(): "21世紀のデータがありません";
+            Console.WriteLine(a);
+
+
+            var years = ym.Select(s => s.AddOneMonth()).OrderBy(y => y.Year);
+            foreach (var b in years) {
+                Console.WriteLine(b);
+            }
+                 
+        }
+        static YearMonth Is21(YearMonth[] yms) {
+            foreach (var i in yms) {
+                if (i.Is21Century) {
+                    return i;
+                }
+            }
+            return null;
+        }
+    }
+}
--- Chapter04/Section01/Program.cs
+++ Chapter04/Section01/Program.cs
@@ -7,6 +7,75 @@ using System.Threading.Tasks;
 namespace Section01 {
     class Program {
         static void Main(string[] args) {
+            //var person = new Person {
+            //    Name = "新井遥菜",
+            //    Birthday = new DateTime(1995, 11, 23),
+            //    PhoneNumber = "123-3456-7890"
+            //};
+
+            //var list = new List<int> { 10, 20, 30, 40, };
+            //var key = 50;
+            //var num = list.Contains(key) ? 1 : 0;
+            //Console.WriteLine(num);
+
+            //string code = "12345";
+            ////null合体演算子
+            //var message = GetMessage(code) ?? DefalutMessage();
+            //Console.WriteLine(message);
+
+            //var ret = GetProductA();
+
+            //int count = 0;
+
+            //Console.WriteLine($"後置 : {count++}");
+
+            //Console.WriteLine($"前置 : {++count}");
+
+            //var str = "123";
+
+            //int height;
+            //if(int.TryParse(str, out var height)) {
+            //    Console.WriteLine(height);
+            //} else {
+            //    Console.WriteLine("変換できません");
+            //}
+
+            //var Session = new Dictionary<string, object>();
+            //Session["MyProduct"] = new Product();
+
+            //var product = Session["MyProduct"] as Product;
+            //if(product == null) {
+            //    Console.WriteLine("productが取得できなかった");
+            //} else {
+            //    Console.WriteLine("productが取得できた");
+            //}
+
+
+
+
+
         }
+
+        //スタブ
+        private static object DefalutMessage() {
+            return "DefalutMessage";
+        }
+
+        private static object GetMessage(object code) {
+            return null;
+        }
+
+        private static Product GetProductA() {
+            Sale sale = new Sale();
+            sale.Product = new Product();
+            sale = null;
+            return sale?.Product; //nill条件演算
+        }
+
+    }
+    class Sale {
+        public string ShopName { get; set; } = "abcde";
+        public int Amount { get; set; } = 12340;
+        public Product Product { get; set; }
     }
 }
