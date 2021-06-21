419eee5e17a7c8fa89e0ed4ecc079f09f5044d23
茂木 洸樹 <32090@ojs.ac.jp>
2021-06-21 14:30:26 +0900

    一期目統一試験　完成

--- TestProject/Test01/ScoreCounter.cs
+++ TestProject/Test01/ScoreCounter.cs
@@ -7,33 +7,57 @@ namespace Test01 {
 
         // コンストラクタ
         public ScoreCounter(string filePath) {
+            _score = ReadScore(filePath);
 
 
 
-            
+
         }
 
 
         //メソッドの概要： 
         private static IEnumerable<Student> ReadScore(string filePath) {
-            
+            List<Student> score = new List<Student>();
+            string[] lines = File.ReadAllLines(filePath);
+            foreach (string line in lines) {
+                string[] items = line.Split(',');
+                Student student = new Student {
+                    Name = items[0],
+                    Sbject = items[1],
+                    Score = int.Parse(items[2])
+                };
+
+                score.Add(student);
+            }
+            return score;
 
 
 
 
 
 
-            
+
+
         }
 
         //メソッドの概要： 
         public IDictionary<string, int> GetPerStudentScore() {
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+            foreach (Student student in _score) {
+                if (dict.ContainsKey(student.Sbject)) {
+                    //すでにコレクションに科目名が追加されている
+                    dict[student.Sbject] += student.Score;
+                } else {
+                    //コレクションへ科目名を登録
+                    dict[student.Sbject] = student.Score;
+                }
+            }
+            return dict;
 
 
 
 
 
-            
         }
     }
 }
--- TestProject/Test01/Student.cs
+++ TestProject/Test01/Student.cs
@@ -1,6 +1,13 @@
 ﻿namespace Test01 {
     class Student {
 
+        public string Name { get;  set; }
+
+        public string Sbject { get; set; }
+
+        public int Score { get; set; }
+
+
 
 
     }
